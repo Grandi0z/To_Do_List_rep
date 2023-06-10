633ea9ec0cf88f3246a9b1215de44b7190f08614
Grandi0z <josephlkhausi@gmail.com>
2023-06-10 19:24:29 +0200

    Fix errors in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -45,10 +45,10 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
       },
 
     ],
