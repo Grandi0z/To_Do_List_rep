cd28f7f88b40d2c5fa699e94dbb5c45fefb0efa0
Grandi0z <josephlkhausi@gmail.com>
2023-06-23 19:35:43 +0200

    Add and remove item in LocalStorage

---
+++ src/modules/test/util.js
@@ -0,0 +1,33 @@
+const localStorageMock = {
+   store:{},
+   clear:jest.fn(),
+   getItem:jest.fn().mockReturnValue([]),
+   setItem:jest.fn(function(key, value) {return this.store[key] = String(value)}),
+   removeItem:jest.fn(function(key) {return delete this.store[key]}),
+   read:function (){return this}
+}
+
+const addTask = (task,parent=false)=>{
+  const tasks = localStorageMock.getItem()
+  tasks.push(task)
+  localStorageMock.setItem("task",tasks)
+  const liElt = document.createElement('li')
+  if(parent){
+    parent.appendChild(liElt)
+  }
+  
+}
+//use task instead of taskId because there is no id in the instances of the mocking class Task
+const deleteTask = (idTask,parent=false) => {
+  const dataTask = localStorageMock.getItem()
+  const newDataTask = dataTask.filter((task) => task.id !== idTask);
+  localStorageMock.setItem("task",newDataTask)
+  if(parent){
+    const liElt = document.getElementById(idTask)
+    liElt.parentNode.removeChild(liElt)
+  }
+};
+
+
+export {localStorageMock, addTask, deleteTask}
+
---
+++ src/modules/test/util.test.js
@@ -0,0 +1,53 @@
+import {localStorageMock, addTask, deleteTask} from "./util.js"
+import Task from "../task.js"
+
+jest.mock('../task.js')
+
+
+
+describe("Save and Delete to Local storage",()=>{
+  afterEach(() => {
+   localStorageMock.setItem('task', []);
+  });
+  it("Should save in local storage",()=>{
+    const task1 = new Task("task 2", false,2)    
+    addTask(task1)
+    expect(localStorageMock.setItem).toHaveBeenCalledWith("task",[task1])
+  });
+  it("Should delete a item in local storage",()=>{
+    const task2 = new Task("task 2", false,2)
+    task2.id=11
+    addTask(task2)  
+    deleteTask(11)
+    //the calls[0] is the first test(it) calls[1] is in afterEach and here the call 2
+    expect(localStorageMock.setItem.mock.calls[2][1]).toContain(task2)
+    expect(localStorageMock.setItem.mock.calls[3][1]).not.toContain(task2)
+  });
+})
+
+describe("Add and a remove a <li> element",()=>{
+  it('should add one new item to the list', () => {
+    document.body.innerHTML =
+    '<div>' +
+    '  <ul id="list"><li></li></ul>' +
+    '</div>';
+    const task3 = new Task("task 4", false, 4)
+    const container = document.querySelector('#list')
+    addTask(task3,container)
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(2);
+ });
+ it('should remove a item to the list', () => {
+  document.body.innerHTML =
+  '<div>' +
+  '  <ul id="list"><li></li> <li id="11"></li></ul>' +
+  '</div>';
+  const task4 = new Task("task 3", false, 3)
+  task4.id="11"
+  addTask(task4)
+  const container = document.querySelector('#list')
+  deleteTask("11",container)
+  const list = document.querySelectorAll('#list li');
+  expect(list).toHaveLength(1);
+});
+})
