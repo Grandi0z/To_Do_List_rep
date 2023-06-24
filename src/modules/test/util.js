eea1ce01fe6a58d9e9319544248d890c0a3f15a7
Grandi0z <josephlkhausi@gmail.com>
2023-06-24 14:31:09 +0200

    fix linters errors

--- src/modules/test/util.js
+++ src/modules/test/util.js
@@ -1,55 +1,56 @@
 const localStorageMock = {
-   store:[],
-   clear:jest.fn(),
-   getItem:jest.fn().mockReturnValue([]),
-   setItem:jest.fn(function(key, value) {return this.store[key] = String(value)}),
-   removeItem:jest.fn(function(key) {return delete this.store[key]}),
-   read:function (){return this}
-}
+  store: [],
+  clear: jest.fn(),
+  getItem: jest.fn().mockReturnValue([]),
+  setItem: jest.fn(function setIt(key, value) {
+    this.store[key] = String(value);
+  }),
+  removeItem: jest.fn(function delIt(key) { delete this.store[key]; }),
+  read() { return this; },
+};
 
-const addTask = (task,parent=false)=>{
-  const tasks = localStorageMock.getItem()
-  tasks.push(task)
-  localStorageMock.setItem("task",tasks)
-  const liElt = document.createElement('li')
-  if(parent){
-    parent.appendChild(liElt)
+const addTask = (task, parent = false) => {
+  const tasks = localStorageMock.getItem();
+  tasks.push(task);
+  localStorageMock.setItem('task', tasks);
+  const liElt = document.createElement('li');
+  if (parent) {
+    parent.appendChild(liElt);
   }
-  
-}
-//use task instead of taskId because there is no id in the instances of the mocking class Task
-const deleteTask = (idTask,parent=false) => {
-  const dataTask = localStorageMock.getItem()
+};
+// use task instead of taskId because there is no id in the instances of the mocking class Task
+const deleteTask = (idTask, parent = false) => {
+  const dataTask = localStorageMock.getItem();
   const newDataTask = dataTask.filter((task) => task.id !== idTask);
-  localStorageMock.setItem("task",newDataTask)
-  if(parent){
-    const liElt = document.getElementById(idTask)
-    liElt.parentNode.removeChild(liElt)
+  localStorageMock.setItem('task', newDataTask);
+  if (parent) {
+    const liElt = document.getElementById(idTask);
+    liElt.parentNode.removeChild(liElt);
   }
 };
 
-const updateTask = (idTask, newContent,parent=false) => {
-  const dataTask = localStorageMock.getItem()
+const updateTask = (idTask, newContent, parent = false) => {
+  const dataTask = localStorageMock.getItem();
   const task = dataTask.find((task) => (task.id === idTask));
   if (newContent.description) {
     task.description = newContent.description;
-    if(parent){
-      const liElt = document.getElementById(idTask)
-      liElt.innerHTML = newContent.description
+    if (parent) {
+      const liElt = document.getElementById(idTask);
+      liElt.innerHTML = newContent.description;
     }
     // if completed === true or === false
   } else if (newContent.completed || !newContent.completed) {
     task.completed = newContent.completed;
   }
-  localStorageMock.setItem("task",dataTask);
+  localStorageMock.setItem('task', dataTask);
 };
 
 const removeCompletedTask = () => {
-  const dataTask = localStorageMock.getItem()
+  const dataTask = localStorageMock.getItem();
   const newDataTask = dataTask.filter((task) => task.completed !== true);
-  localStorageMock.setItem("task",newDataTask);
+  localStorageMock.setItem('task', newDataTask);
 };
 
-
-export {localStorageMock, addTask, deleteTask, updateTask, removeCompletedTask}
-
+export {
+  localStorageMock, addTask, deleteTask, updateTask, removeCompletedTask,
+};
